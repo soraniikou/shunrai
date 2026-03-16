23edb216da07312b94a6549185b47a9b30644f64
soraniikou <maroyuki728@gmail.com>
2026-03-16 11:58:19 +0900

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Shunrai() {
   const [text, setText] = useState("");
